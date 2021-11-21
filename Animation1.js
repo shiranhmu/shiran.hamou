a6a2bc173e90f0f921bf5685bab0f0cc680642b4
shiranhmu <shiranhmu@gmail.com>
2021-11-21 16:11:05 +0200

    ex5

---
+++ Animation1.js
@@ -0,0 +1,11 @@
+function MyName(){
+      document.getElementById("Button").innerHTML = "Shiran Hamou";
+      console.log("Shiran Hamou");
+}
+
+var x=5, y=7,T=(x==y);
+console.log(T);
+
+function changePic(){
+    document.getElementById("HelAnimation").src = "IMG-20191012-WA0039.JPG";
+}
